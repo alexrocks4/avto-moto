23ae8043bc90b62a1d547d19b1b527750c572d32
alex <sakun.alexey@gmail.com>
2021-10-12 19:17:37 +0300

    refactor: move mock data to the dedicated directory

--- src/components/characteristics/characteristics.jsx
+++ src/components/characteristics/characteristics.jsx
@@ -1,46 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import { characteristicsData } from '../../mocks/characteristics';
 import styles from './characteristics.module.scss';
-import { nanoid } from '@reduxjs/toolkit';
-
-const characteristicsData = [
-  {
-    id: nanoid(),
-    name: 'Трансмиссия',
-    data: 'Роботизированная',
-  },
-  {
-    id: nanoid(),
-    name: 'Мощность двигателя, л.с.',
-    data: '249',
-  },
-  {
-    id: nanoid(),
-    name: 'Тип двигателя',
-    data: 'Бензиновый',
-  },
-  {
-    id: nanoid(),
-    name: 'Привод',
-    data: 'Полный',
-  },
-  {
-    id: nanoid(),
-    name: 'Объем двигателя, л',
-    data: '2.4',
-  },
-  {
-    id: nanoid(),
-    name: 'Макс. крутящий момент',
-    data: '370/4500',
-  },
-  {
-    id: nanoid(),
-    name: 'Количество цилиндров',
-    data: '4',
-  },
-];
 
 function Characteristics({ className }) {
   return (
